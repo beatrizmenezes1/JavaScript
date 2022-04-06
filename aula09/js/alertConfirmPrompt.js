ed185ae6506b8c671c6f661312810855202be3fb
beatrizmenezes1 <beatrizmz.26menezes@gmail.com>
2022-04-06 11:32:13 -0300

    [aula09] [Aula09 e ex001]

---
+++ aula09/js/alertConfirmPrompt.js
@@ -0,0 +1,4 @@
+//Qunado há parenteses, isso é uma função/metodo que irá realizar uma ação
+window.alert('Bem vindo BEA!'); // um caixa de alerta na tela
+window.confirm("Confirmação") // uma caixa para confirmação
+window.prompt("Você realmete está feliz?") // uma caixa que pode inserir texto
