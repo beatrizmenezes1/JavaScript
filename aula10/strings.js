e29415fa5c62041d6ce84b104ef63d6ed0adcdae
beatrizmenezes1 <beatrizmz.26menezes@gmail.com>
2022-04-07 08:18:24 -0300

    resolvendo problemas

---
+++ aula10/strings.js
@@ -0,0 +1,9 @@
+// let umaString = "Um \"texto\"";
+// console.log(umaString)
+
+//Cada string tem um indice
+//               01234567
+let umaString = "Um texto";
+//console.log(umaString[4])
+//console.log(umaString.charAt(6));
+//console.log(umaString.concat(" ", "em", " ", "um"));
