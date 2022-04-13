4ca4aae4943d20c103a7cc4a140a6e63d5629d7a
beatrizmenezes1 <beatrizmz.26menezes@gmail.com>
2022-04-13 13:44:21 -0300

    aula11

---
+++ aula11/maisNumeros.js
@@ -0,0 +1,33 @@
+//  IEEE 754-2008
+let num1 = 0.7; //number
+let num2 = 0.1; //number
+
+// num1 += num2; // num1 = num1 + num2; 0.8
+// num1 += num2; // 0.9
+// num1 += num2; // 1.0
+// num1 += num2;
+// num1 = Number(num1.toFixed(2));
+
+// console.log(num1)  
+// console.log(Number.isInteger(num1));
+
+num1 = ((num1 * 100) + (num2 * 100))/100;
+num1 = ((num1 * 100) + (num2 * 100))/100;
+num1 = ((num1 * 100) + (num2 * 100))/100;
+console.log(num1)  
+console.log(Number.isInteger(num1));
+
+//nao quero somar, eu quero concatenar
+// console.log(num1 + num2);
+
+//num1 = num1.toString();
+
+//console.log(num1.toString(2)); // para ver esse numero de forma binaria
+
+//console.log(num1.toFixed(2)); //define as casas decimais
+
+//console.log(Number.isInteger(num1)); // vai retornar true ou false. Se o numero é ou nao um numero inteiro.
+
+// let temp = num1 * 'olá';
+// console.log(Number.isNaN(temp)); //vai retornar verdadeiro sempre que a conta for invalida
+
